5939dd4491d94725926b3e2c099ea77f3cc26a20
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-05-11 19:56:27 -0400

    test passing, added api call

--- src/containers/HeyArnoldContainer.jsx
+++ src/containers/HeyArnoldContainer.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import {findCharacters} from '../services/heyArnoldApi';
 
 export default class HeyArnoldContainer extends Component {
+    state = {
+        loading: true, 
+        characters: []
+    }
+
+    async componentDidMount() {
+        const characters = await findCharacters(); 
+        this.setState({
+            characters,
+            loading: false,
+        });
+    }
+
     render() {
-        return (
-            <h1>
-                hi 
-            </h1>
-        )
+        const { loading, characters } = this.state; 
+
+        if (loading) return <h1>Loading...</h1>;
+
+        return <ul aria-label="characters"></ul>
+
     }
 }
---
+++ src/services/heyArnoldApi.js
@@ -0,0 +1,12 @@
+export const findCharacters = async () => {
+  const res = await fetch(
+    'https://hey-arnold-api.herokuapp.com/api/v1/characters'
+  );
+  const results = await res.json();
+
+  return results.map((character) => ({
+    id: character.id,
+    name: character.name,
+    image: character.image,
+  }));
+};
